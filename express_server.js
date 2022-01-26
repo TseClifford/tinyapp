eb7971fedabc8eae2ece9d03cbde88f55fd8612d
Clifford Tse <TseClifford@gmail.com>
2022-01-26 23:18:30 +0000

    remove old db

--- express_server.js
+++ express_server.js
@@ -42,11 +42,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com",
-// };
-
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
